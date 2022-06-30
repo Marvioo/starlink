f2c48a38e2575686cb911d8ac9927527d6918fb3
Marvioo <guangjia@usc.edu>
2022-06-30 11:32:40 -0700

    mode changes

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+// export const SAT_API_KEY = "A6G3MD-ZPKNDQ-2MTPP9-4U9X";
+export const SAT_API_KEY = "7CT349-BAFP43-PXJQ2Y-4VUR";
+
+export const STARLINK_CATEGORY = "52";
+
+export const NEARBY_SATELLITE = "rest/v1/satellite/above";
+
+export const BASE_URL = "https://18.116.200.182";
+
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
